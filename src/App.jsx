bf5b3767349b579f9bca0fc09eee5215087e7eed
Fynn <fynn.young@gearset.com>
2024-08-12 14:36:22 +0100

    create `NoteTitle` component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+import { useRef } from "react";
+
+import "./App.css";
 function App() {
 	return (
 		<>
 			<div>
 			</div>
+
+function NoteTitle({ title }) {
+	const [noteTitle, setNoteTitle] = useState(title);
+	const inputRef = useRef(null);
+
+	const handleBlur = () => {
+		if (inputRef.current) {
+			inputRef.current.setSelectionRange(0, 0);
+		}
+	};
+
+	return (
+		<input
+			className="title-input"
+			value={noteTitle}
+			onChange={(e) => setNoteTitle(e.target.value)}
+			maxLength={42}
+			onBlur={handleBlur}
+			ref={inputRef}
+		></input>
+	);
+}
 		</>
 	);
 }
