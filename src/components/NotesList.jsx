c047d8f444cf8187521b241914ccbd563589e76a
Fynn <fynn.young@gearset.com>
2024-08-16 11:18:21 +0100

    create methods for setting title & content

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -11,6 +11,28 @@ function NotesList({ notes, setNotes }) {
 		setNotes((prevNotes) => prevNotes.filter((note) => note.id !== id));
 	};
 
+	const setNoteTitle = (id, newTitle) => {
+		setNotes((prevNotes) =>
+			prevNotes.map((note) => {
+				if (note.id === id) {
+					return { ...note, title: newTitle };
+				}
+				return note;
+			})
+		);
+	};
+
+	const setNoteContent = (id, newContent) => {
+		setNotes((prevNotes) =>
+			prevNotes.map((note) => {
+				if (note.id === id) {
+					return { ...note, content: newContent };
+				}
+				return note;
+			})
+		);
+	};
+
 	return (
 		<div className="notes-list">
 			{notes.map((note) => (
@@ -18,9 +40,12 @@ function NotesList({ notes, setNotes }) {
 					key={note.id}
 					id={note.id}
 					title={note.title}
+					content={note.content}
 					maxZIndex={maxZIndex}
 					setMaxZIndex={setMaxZIndex}
 					removeNote={removeNote}
+					setNoteTitle={setNoteTitle}
+					setNoteContent={setNoteContent}
 				/>
 			))}
 		</div>
