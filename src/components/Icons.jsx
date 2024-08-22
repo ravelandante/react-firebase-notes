670475baa89500fe0dd2b4a4b07f81bf5013ff22
Fynn <fynn.young@gearset.com>
2024-08-22 16:59:43 +0100

    add `Icons` component

---
+++ src/components/Icons.jsx
@@ -0,0 +1,43 @@
+export const DeleteIcon = () => {
+	return (
+		<svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path
+				d="M16 2v4h6v2h-2v14H4V8H2V6h6V2h8zm-2 2h-4v2h4V4zm0 4H6v12h12V8h-4zm-5 2h2v8H9v-8zm6 0h-2v8h2v-8z"
+				fill="currentColor"
+			/>
+		</svg>
+	);
+};
+
+export const EditIcon = () => {
+	return (
+		<svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path
+				d="M18 2h-2v2h2V2zM4 4h6v2H4v14h14v-6h2v8H2V4h2zm4 8H6v6h6v-2h2v-2h-2v2H8v-4zm4-2h-2v2H8v-2h2V8h2V6h2v2h-2v2zm2-6h2v2h-2V4zm4 0h2v2h2v2h-2v2h-2v2h-2v-2h2V8h2V6h-2V4zm-4 8h2v2h-2v-2z"
+				fill="currentColor"
+			/>
+		</svg>
+	);
+};
+
+export const TickIcon = () => {
+	return (
+		<svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path
+				d="M18 6h2v2h-2V6zm-2 4V8h2v2h-2zm-2 2v-2h2v2h-2zm-2 2h2v-2h-2v2zm-2 2h2v-2h-2v2zm-2 0v2h2v-2H8zm-2-2h2v2H6v-2zm0 0H4v-2h2v2z"
+				fill="currentColor"
+			/>
+		</svg>
+	);
+};
+
+export const NewNoteIcon = () => {
+	return (
+		<svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<path
+				d="M7 1H5v3H2v2h3v3h2V6h3V4H7V1zm12 1h-7v2h7v10h-6v6H5v-9H3v11h12v-2h2v-2h2v-2h2V2h-2zm-2 16h-2v-2h2v2z"
+				fill="currentColor"
+			/>
+		</svg>
+	);
+};
